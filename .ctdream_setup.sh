9db7f60070ba53a30b1584e82d89aaf533144120
Yash Parghi <yash@daybydaycoding.com>
2019-10-09 16:35:47 -0400

    Save my shell env/eval

---
+++ .ctdream_setup.sh
@@ -0,0 +1,8 @@
+# nvm: copied manually here by Yash from .bashrc after running nvm's install
+# script
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+eval "$(rbenv init -)"
+
