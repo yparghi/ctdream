22c4de6e120274e7e77fcb5e9daf4265331f151f
Yash Parghi <yash@daybydaycoding.com>
2019-10-09 17:40:01 -0400

    Untested: Try stubbing in a React app

---
+++ react_only_app/YashApp.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+
+import "./YashApp.css";
+
+
+class YashApp extends Component{
+  render(){
+    return(
+      <div className="YashApp">
+        <h1> Hello, World! </h1>
+      </div>
+    );
+  }
+}
+
---
+++ react_only_app/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import YashApp from "./YashApp.js";
+
+
+ReactDOM.render(<YashApp />, document.getElementById("root"));
+
