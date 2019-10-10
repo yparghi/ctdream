54d701ea022b273a080c929e9c98f22da1cfa72e
Yash Parghi <yash@daybydaycoding.com>
2019-10-10 15:34:51 -0400

    Some early practice using components

--- react_only_app/src/YashApp.js
+++ react_only_app/src/YashApp.js
@@ -1,11 +1,37 @@
 import React, { Component } from "react";
 
 
+class TestOtherComponent extends Component {
+  render(){
+    return(
+      <div>
+        <h3> Hello, World! </h3>
+        <SwitchHeaderButton switchSelector="Mangoes"/>
+      </div>
+    );
+  }
+}
+
+class SwitchHeaderButton extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <button>Hello {this.props.switchSelector}!</button>
+      </div>
+    );
+  }
+}
+
 class YashApp extends Component {
   render(){
     return(
       <div className="YashAppClass">
-        <h1> Hello, World! </h1>
+          <TestOtherComponent/>
       </div>
     );
   }
